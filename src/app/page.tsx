0fc5956e4cdc2f1209d56651ceb045c58be7a9ce
SibomanaEdouard <sibomanaedouard974@gmail.com>
2024-11-13 02:18:59 +0200

    final codes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
+
 import { useState, useEffect } from 'react';
 import GoogleMap from '../components/GoogleMap';
 import markersData from '../data/SiteMarkers.json'; 
@@ -20,6 +21,7 @@ export default function Home() {
       console.error('Error loading data:', error);
     }
   }, []);
+  
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4 text-center">Google Maps Integration</h1>
