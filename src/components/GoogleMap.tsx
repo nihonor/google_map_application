eb2bc44a8eac9a8199028a70edcf729ec3e790b8
SibomanaEdouard <sibomanaedouard974@gmail.com>
2024-12-14 17:20:56 +0200

    updates

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useRef, useState } from 'react';
 import { loader, getLatLngFromAddress, parseLatLng, reverseGeocode } from '@/app/utils/MapUtils';
 import { SiteMarker, InterConnectSegment, Address } from '@/types';
